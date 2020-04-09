fd6f22d4c35f1f862f1bf9698d1c4255736fc14b
yoitsu05 <yoshitaka.oitsu@plaid.co.jp>
2020-04-09 12:02:46 +0900

    fix

--- src/components/Reading.vue
+++ src/components/Reading.vue
@@ -20,14 +20,29 @@
 
     <v-layout align-center>
       <v-flex xs3>
-        <base-btn v-if="page !== 1" class="ml-0" title="Previous page" square @click="page--">
+        <base-btn
+          v-if="page !== 1"
+          class="ml-0"
+          title="Previous page"
+          square
+          @click="page--"
+        >
           <v-icon>mdi-chevron-left</v-icon>
         </base-btn>
       </v-flex>
 
-      <v-flex xs6 text-xs-center subheading> PAGE {{ page }} OF {{ pages }} </v-flex>
+      <v-flex
+        xs6
+        text-xs-center
+        subheading
+      >
+        PAGE {{ page }} OF {{ pages }}
+      </v-flex>
 
-      <v-flex xs3 text-xs-right>
+      <v-flex
+        xs3
+        text-xs-right
+      >
         <base-btn
           v-if="pages > 1 && page < pages"
           class="mr-0"
@@ -45,10 +60,11 @@
 <script>
 // Utilities
 import { mapState } from 'vuex';
+import ReadingCard from '@/components/ReadingCard';
 
 export default {
   components: {
-    ReadingCard: () => import('@/components/ReadingCard'),
+    ReadingCard
   },
 
   data: () => ({
